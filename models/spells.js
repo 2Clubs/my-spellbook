7113f1a028647e7d9af62b4fc2161a74d753b56a
2clubs <bennettdan85@gmail.com>
2023-04-10 09:21:51 -0400

    adds spell schema

---
+++ models/spells.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const spellSchema = new Schema(
+    {
+        name: String,
+        level: Number,
+        description: String,
+        school: String,
+        materials: String,
+        components: String,
+        castingTime: String,
+        duration: String,
+    },
+    {timestamps: true}
+)
+
+const Spell = mongoose.model('Spell', spellSchema)
+
+module.exports = Spell
