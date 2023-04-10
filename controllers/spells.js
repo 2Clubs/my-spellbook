7a30c760ab853901ca9289df86b87e2ee9b2d07b
2clubs <bennettdan85@gmail.com>
2023-04-10 12:13:39 -0400

    working index.ejs

--- controllers/spells.js
+++ controllers/spells.js
@@ -2,9 +2,11 @@ const express = require('express')
 const spellsRouter = express.Router()
 const Spell = require('../models/spells')
 
-spellsRouter.get('/', async (req, res) =>{
+spellsRouter.get('/', async (req, res) => {
     const allSpells = await Spell.find({})
     res.render('index.ejs', {
         spells: allSpells,
     })
 })
+
+module.exports = spellsRouter
