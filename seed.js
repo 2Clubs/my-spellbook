f98e2e76ee395ee1c9f9fc64257f574b2d329172
2clubs <bennettdan85@gmail.com>
2023-04-10 09:49:37 -0400

    adds seed files

---
+++ seed.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Spell = require('./models/spells')
+const spellArr = require('./spellsArr')
+require('dotenv').config
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const seedSpellsDB = async() => {
+    await Spell.deleteMany({})
+    await Spell.insertMany(spellArr)
+}
+
+seedSpellsDB().then(() => {
+    mongoose.connection.close()
+})
