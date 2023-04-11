4a268c6417313b83194fdd873d5cdd3dcc73b427
2clubs <bennettdan85@gmail.com>
2023-04-11 15:34:31 -0400

    renders dashboard.ejs

--- controllers/spells.js
+++ controllers/spells.js
@@ -6,8 +6,9 @@ const Spell = require('../models/spells')
 // Index
 spellsRouter.get('/', async (req, res) => {
     const allSpells = await Spell.find({})
-    res.render('index.ejs', {
+    res.render('dashboard.ejs', {
         spells: allSpells,
+        currentUser: req.session.currentUser
     })
 })
 
