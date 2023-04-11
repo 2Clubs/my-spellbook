15080001b0a71ba611e576774da8e1ce790e353e
2clubs <bennettdan85@gmail.com>
2023-04-11 10:42:47 -0400

    adds comments

--- controllers/spells.js
+++ controllers/spells.js
@@ -1,12 +1,16 @@
+// Dependencies
 const express = require('express')
 const spellsRouter = express.Router()
 const Spell = require('../models/spells')
 
+// Index
 spellsRouter.get('/', async (req, res) => {
     const allSpells = await Spell.find({})
     res.render('index.ejs', {
         spells: allSpells,
     })
 })
+
+
 
 module.exports = spellsRouter
